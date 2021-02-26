0a266da3e7baac81856162aad14e511cabfc5f33
Otavio Pellicano <otavio.pellicano@gmail.com>
2021-02-25 22:08:01 -0300

    Create Search Dialog

---
+++ SearchDialog.cpp
@@ -0,0 +1,14 @@
+#include "SearchDialog.hpp"
+#include "ui_SearchDialog.h"
+
+SearchDialog::SearchDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SearchDialog)
+{
+    ui->setupUi(this);
+}
+
+SearchDialog::~SearchDialog()
+{
+    delete ui;
+}
